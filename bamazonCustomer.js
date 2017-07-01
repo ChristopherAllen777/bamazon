772dec8f0dca696923979bcd13bec7f1af6a135a
Chris Allen <christopherallen777@gmail.com>
2017-07-01 15:12:07 -0500

    Setting up bamazon node.js files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
+// --NPM Dependencies Packages-- //
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+// --Set up NPM Package install and Database Connection-- //
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -13,20 +14,61 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
+// --Connection TEST to database --//
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  console.log("");
+  console.log("---- WELCOME TO BAMAZON! ----")
+  console.log("");
 });
 
-connection.query("SELECT * FROM songs", function(err, res) {
-  for (var i = 0; i < res.length; i++) {
-    console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
-  }
-  console.log("-----------------------------------");
-});
+// -- Query Test -- //
+connection.query("SELECT * FROM products", function(err, res) {
+  if (err) throw err;
+  console.log(res);
+})
 
-connection.query("SELECT * FROM songs WHERE genre=?", ["Dance"], function(err, res) {
-  for (var i = 0; i < res.length; i++) {
-    console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
-  }
-});
+inquirer.prompt([
+    {
+      type: "list",
+      message: "Choose your flashcard Game.",
+      choices: ["Basic", "Cloze"],
+      name: "choice"
+    }
+  ]).then(function(user) {
+    var inputResults = user.choice;
+    console.log(inputResults);
+
+    switch (inputResults) {
+
+      case "Basic":
+      askQuestionb();
+      break;
+
+      case "Cloze":
+      askQuestion();
+      break;
+
+    default:
+          console.log("error");
+    }
+  });
+
+
+
+
+
+
+// connection.query("SELECT * FROM songs", function(err, res) {
+//   for (var i = 0; i < res.length; i++) {
+//     console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].genre);
+//   }
+//   console.log("-----------------------------------");
+// });
+
+// connection.query("SELECT * FROM songs WHERE genre=?", ["Dance"], function(err, res) {
+//   for (var i = 0; i < res.length; i++) {
+//     console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+//   }
+// });
