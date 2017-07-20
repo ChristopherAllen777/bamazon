705bf0c451ee306ea5f3c5b8974f92541b5699b1
Chris Allen <christopherallen777@gmail.com>
2017-07-20 17:41:12 -0500

    Update to sql schema file

--- bamazon.sql
+++ bamazon.sql
@@ -1,34 +1,3 @@
--- CREATE DATABASE greatbay_db;
--- 
--- USE greatbay_db;
--- 
--- CREATE TABLE items (
---  id INT NOT NULL AUTO_INCREMENT,
---  Item VARCHAR(45) NULL,
---  price DECIMAL(10,2) NULL,
---  quantity INT NULL,
---  PRIMARY KEY (id)
--- );
--- 
--- INSERT INTO items (Item, price, quantity)
--- VALUES ("Boots", 2.50, 20);
--- 
--- INSERT INTO items (Item, price, quantity)
--- VALUES ("Hats", 3.10, 5);
--- 
--- INSERT INTO items (Item, price, quantity)
--- VALUES ("Shoes", 3.25, 15);
-
--- USE greatbay_db;
--- ALTER TABLE items CHANGE price starting_bid char(50);
--- ALTER TABLE items CHANGE quantity buy_it_now char(50);
-
--- DROP TABLE items;
-
--- -------------------------------------------------------------
--- BAMAZON HOMEWORK -- 
--- -------------------------------------------------------------
--- 
 CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
@@ -41,38 +10,20 @@ CREATE TABLE products (
  stock_quantity INT NOT NULL,
  PRIMARY KEY (item_id)
 );
--- 
+
+-- This is to drop the table if needed. Query the below line
 -- DROP TABLE products
 
+SELECT * FROM products;
+
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Xbox One", "electronics", 300, 400), ("Air Jordans", "apparel", 125, 232), ("Sweatshirt", "apparel", 50, 132),
-("Camera", "electronics", 800, 50), ("Phone", "electronics", 600, 22), ("Lazyboy", "furniture", 350, 77), ("Books", "Books", 23, 500),
-("Football", "sports", 17, 38), ("Shotgun", "outdoors", 230, 30), ("Lamp", "home", 66, 75);
-        
--- 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Air Jordans", "apparel", 125, 232);
--- 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Sweatshirt", "apparel", 50, 132);
--- 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Camera", "electronics", 800, 50);
--- 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Phone", "electronics", 600, 22);
--- 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Lazyboy", "furniture", 350, 77);
--- 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Books", "Books", 23, 500);
--- 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Football", "sports", 17, 38);
--- 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Shotgun", "outdoors", 230, 30);
--- 
--- INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Lamp", "home", 66, 75);
+VALUES ("Uncharted 4", "Video Games", 49.95, 150),
+  ("DOOM", "Video Games", 59.99, 200),
+  ("Crate of Spam", "Food and Drink", 24.50, 50),
+  ("Cool Shades", "Apparel", 75.00, 5),
+  ("Worn Denim Jeans", "Apparel", 54.25, 35),
+  ("Survival Towel", "Necessities", 42.42, 42),
+  ("Bill and Ted's Excellent Adventure", "Films", 15.00, 25),
+  ("Mad Max: Fury Road", "Films", 25.50, 57),
+  ("Monopoly", "Board Games", 30.50, 35),
+  ("Yahtzee", "Board Games", 19.95, 23);
